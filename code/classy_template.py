19014c8b4c145d39d23a50be75b5fc3daa80cabb
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-12-19 12:53:11 +0100

    Update classy_template.py

--- code/classy_template.py
+++ code/classy_template.py
@@ -3,11 +3,11 @@ from typing import *
 class Example:
     def __init__(self,test:int):
         self.test=test
+    def testfn(self,v):
+        return v**2+self.test
 
 
 MAINCLASS=Example
-
-fn_map=
 
 def testfn(functions:list[str],args:list[list]):
     functions.reverse()
@@ -18,23 +18,17 @@ def testfn(functions:list[str],args:list[list]):
     res=[]
     while functions:
         c_fn=functions.pop()
-        fn=fn_map.get(c_fn,print)
+        fn=MAINCLASS.__getattribute__(cls,c_fn)
         c_args=args.pop()
-        cures=fn(cls,*c_args)
+        cures=fn(*c_args)
         res.append(cures)
     return res
 
 TESTS = [
     (
-        ["MovieRentingSystem","search","rent","rent","report","drop","search"],
-        [[3,[[0,1,5],[0,2,6],[0,3,7],[1,1,4],[1,2,7],[2,1,5]]],[1],[0,1],[1,2],[],[1,2],[2]],
-        [[1, 0, 2], None, None, [[0, 1], [1, 2]], None, [0, 1]]
-    )
-    ,
-    (
-        ["MovieRentingSystem","rent","drop","rent","rent","rent","drop","search","report","rent","search"],
-[[10,[[4,374,55],[1,6371,21],[8,3660,24],[1,56,32],[5,374,71],[3,4408,36],[6,9322,73],[6,9574,92],[8,7834,62],[2,6084,27],[7,3262,89],[2,8959,53],[0,3323,41],[6,6565,45],[0,4239,20]]],[0,4239],[0,4239],[3,4408],[2,6084],[0,4239],[0,4239],[9346],[],[6,9322],[8698]],
-        []
+        ["","testfn"],
+        [[1],[2]],
+        [5]
     )
 ]
 
