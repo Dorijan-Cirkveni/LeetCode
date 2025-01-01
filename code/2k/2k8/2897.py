b0cd83f5e3010e1bdd57e8a0ff7ec1004a300e20
Mouse Potato Does Stuff <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2025-01-01 15:39:46 +0100

    Update 2897.py

--- code/2k/2k8/2897.py
+++ code/2k/2k8/2897.py
@@ -1,39 +1,75 @@
+import math
 from typing import *
 import inspect
 
 
 def numToNewBitCounter(n):
-    L=[]
+    L = []
     while n:
-        n,b=divmod(n,2)
-        L.append(n)
+        n, b = divmod(n, 2)
+        L.append(b)
     return L
-def numToOldBitCounter(n,L):
-    i=0
-    n,b=divmod(n,2)
-    L[i]+=b
-    i+=1
-def oldBitCounterToCurnum(L):
-    A=L[:]
+
+
+def numToOldBitCounter(n, L):
+    i = 0
+    while n:
+        n, b = divmod(n, 2)
+        L[i] += b
+        i += 1
+
+
+def shaveCounter(L, k):
+    res = 0
+    for i, e in enumerate(L):
+        if not e:
+            continue
+        L[i] -= k
+        res |= 1 << i
+    return res
+
+
+def oldBitCounterToSquareSum(L,lim:int):
+    A = list(set(L))
+    A.sort(reverse=True)
+    last = 0
+    res = 0
+    while A and lim:
+        while not L[-1]:
+            L.pop()
+        cur = A.pop()
+        delta = cur - last
+        if delta>lim:
+            delta=lim
+        last = cur
+        n = shaveCounter(L, delta)
+        sqr=n*n*delta
+        res += sqr
+        lim-=delta
+    return res
+
+
 class Solution:
     def maxSum(self, nums: List[int], k: int) -> int:
         nums.sort()
-        counts=numToNewBitCounter(nums.pop())
+        counts = numToNewBitCounter(nums.pop())
         while nums:
-            numToOldBitCounter(nums.pop(),counts)
+            numToOldBitCounter(nums.pop(), counts)
+        res = oldBitCounterToSquareSum(counts,k)
+        return res%(10**9+7)
 
     main = maxSum
 
 
 TESTS = [
     (
-        ([0, 1], 1),
-        "test"
+        ([2,6,5,8], 2),
+        261
     )
     ,
     (
-        ([0, 1], 2),
-        "also test"
+        ([4,5,4,7], 3),
+        90
     )
 ]
 
