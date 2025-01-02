11674e61768140b8ec7246088d7ede0d976520df
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2025-01-02 08:08:19 +0100

    I will have order

--- code/2k/2k5/2559.py
+++ code/2k/2k5/2559.py
@@ -2,29 +2,49 @@ from typing import *
 import inspect
 
 
+vowelset=set('aeiou')
+def isvowel(word):
+    if word[0] not in vowelset:
+        return 0
+    if word[-1] not in vowelset:
+        return 0
+    return 1
+def reverse_acc_vowel(words):
+    words.reverse()
+    acc=0
+    vowel_acc=[]
+    while words:
+        cur=words.pop()
+        acc+=isvowel(cur)
+        vowel_acc.append(acc)
+    vowel_acc.append(0)
+    return vowel_acc
+def process_queries(vowel_acc,queries):
+    queries.reverse()
+    res=[]
+    while queries:
+        a,b=queries.pop()
+        cur=vowel_acc[b]-vowel_acc[a-1]
+        res.append(cur)
+    return res
 class Solution:
-    """
-    Solulu.
-    """
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowel_acc=reverse_acc_vowel(words)
+        res=process_queries(vowel_acc,queries)
+        return res
 
-    def __init__(self):
-        self.test = "test"
-
-    def Template(self, L: List, i: int):
-        return self.test
-
-    main = Template
+    main = vowelStrings
 
 
 TESTS = [
     (
-        ([0, 1], 1),
-        "test"
+        (["aba","bcb","ece","aa","e"], [[0,2],[1,4],[1,1]]),
+        [2,3,0]
     )
     ,
     (
-        ([0, 1], 2),
-        "also test"
+        (["a","e","i"], [[0,2],[0,1],[2,2]]),
+        [3,2,1]
     )
 ]
 
