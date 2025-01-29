a76281fad669e21a4c38009df6bcecfb19a822d6
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2025-01-29 06:45:29 +0100

    Create DynamicGroup.py

---
+++ code/patterns/DynamicGroup.py
@@ -0,0 +1,15 @@
+class DynamicGroupList:
+    def __init__(self,size:int):
+        self.lead=[i for i in range(size)]
+
+    def flatten(self,cur):
+        l=self.lead
+        while l[cur]!=l[nex]
+
+
+def main():
+    print("This is a template.")
+
+
+if __name__ == "__main__":
+    main()
